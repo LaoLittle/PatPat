5bf5b1f0068fba7abaff051644a8e1ebca7614a4
LaoLittle <y1941803875@outlook.com>
2021-10-30 18:11:58 +0800

    清理缓存文件

---
+++ src/main/kotlin/command/ClearCache.kt
@@ -0,0 +1,23 @@
+package org.laolittle.plugin.command
+
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.PlainText
+import org.laolittle.plugin.PatPat
+import org.laolittle.plugin.PatPat.dataFolder
+import java.io.File
+
+object ClearCache : SimpleCommand(
+    PatPat, "clearcache", "cc",
+    description = "清理缓存"
+){
+    @Handler
+    suspend fun CommandSender.handle(){
+        val tmp = File("$dataFolder/tmp")
+        var flag = false
+        if(tmp.exists()) flag = tmp.deleteRecursively()
+        if(flag) sendMessage(At(user!!) + PlainText("缓存清理完毕"))
+        else sendMessage(PlainText("缓存清理失败！"))
+    }
+}
