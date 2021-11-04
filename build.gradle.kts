465bcb380b57e3996dacd06ef7acad42fc71bb11
LaoLittle <y1941803875@outlook.com>
2021-11-04 23:49:29 +0800

    更改版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "org.laolittle.plugin"
-version = "1.0.4"
+version = "1.0.5"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
--- src/main/kotlin/PatPat.kt
+++ src/main/kotlin/PatPat.kt
@@ -15,7 +15,7 @@ object PatPat : KotlinPlugin(
     JvmPluginDescription(
         id = "org.laolittle.plugin.PatPat",
         name = "PatPat",
-        version = "1.0.4",
+        version = "1.0.5",
     )
 ) {
     override fun onEnable() {
