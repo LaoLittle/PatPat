265e302f55f47e3a1a3ac3848e76c56ece15b33b
LaoLittle <y1941803875@outlook.com>
2021-12-18 16:39:44 +0800

    修改提示语

--- src/main/kotlin/command/Pat.kt
+++ src/main/kotlin/command/Pat.kt
@@ -3,13 +3,14 @@ package org.laolittle.plugin.command
 import net.mamoe.mirai.console.command.CommandSenderOnMessage
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
-import net.mamoe.mirai.console.command.getGroupOrNull
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.contact.Contact.Companion.sendImage
-import net.mamoe.mirai.contact.User
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.contact.Member
+import net.mamoe.mirai.message.data.content
 import org.laolittle.plugin.PatPat
 import org.laolittle.plugin.PatPat.dataFolder
-import org.laolittle.plugin.model.getPat
+import org.laolittle.plugin.model.PatPatTool.getPat
 import java.io.File
 
 @ConsoleExperimentalApi
@@ -22,8 +23,17 @@ object Pat : SimpleCommand(
     override val prefixOptional: Boolean = true //命令可免去斜杠
 
     @Handler
-    suspend fun CommandSenderOnMessage<*>.handle(target: User){
+    suspend fun CommandSenderOnMessage<*>.handle(target: Member? = null){
+        val group = fromEvent.subject
+        if (group !is Group){
+            group.sendMessage("只有在群聊里才可以摸指定的人哦")
+            return
+        }
+        if (target == null) {
+            group.sendMessage("请输入 ${fromEvent.message.content}+@ 来摸一个人")
+            return
+        }
         getPat(target, 80)
-        getGroupOrNull()?.sendImage(File("$dataFolder/tmp").resolve("${target.id}_pat.gif"))
+        group.sendImage(File("$dataFolder/tmp").resolve("${target.id}_pat.gif"))
     }
 }
